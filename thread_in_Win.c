8933c59747170b9c66ef96368c0da9a1b29357ce
fahdolaf <46709034+fahdolaf@users.noreply.github.com>
2020-11-23 23:38:07 +0200

    Create thread_in_Win.c

---
+++ thread_in_Win.c
@@ -0,0 +1,25 @@
+/*
+ ============================================================================
+ Name        : C_Play.c
+ Author      : Ahmed Elghamry
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main() {
+
+	printf("\n========= Main ============\n" );
+
+	
+  
+  printf("Thread execution finished, exiting from main program\n");
+  return 0; 
+}
