c5ce5a058105242b418634f3209b0563c73d7499
fahdolaf <46709034+fahdolaf@users.noreply.github.com>
2020-11-24 00:59:20 +0200

    Update thread_in_Win_2.cpp

--- thread_in_Win_2.cpp
+++ thread_in_Win_2.cpp
@@ -6,66 +6,37 @@
 
  
 
-DWORD WINAPI MyThreadFunction(LPVOID lpParam)
-
-{
-
-printf("The parameter: %u.\n", *(DWORD*)lpParam);
-
+DWORD WINAPI MyThreadFunction(LPVOID lpParam){
+printf("The parameter: %lu.\n", *(DWORD*)lpParam);
 return 0;
-
 }
 
  
 
-int main(void)
-
-{
-
+int main(void){
 DWORD dwThreadId, dwThrdParam = 1;
-
 HANDLE hThread;
-
- 
-
 hThread = CreateThread(
-
 NULL, // default security attributes
-
 0, // use default stack size
-
 MyThreadFunction, // thread function
-
 &dwThrdParam, // argument to thread function
-
 0, // use default creation flags
-
 &dwThreadId); // returns the thread identifier
 
- 
-
-printf("The thread ID: %d.\n", dwThreadId);
-
- 
+printf("The thread ID: %ld.\n", dwThreadId);
 
 // Check the return value for success. If something wrong...
-
 if (hThread == NULL)
-
-printf("CreateThread() failed, error: %d.\n", GetLastError());
-
+printf("CreateThread() failed, error: %ld.\n", GetLastError());
 //else, gives some prompt...
-
 else
-
 printf("It seems the CreateThread() is OK lol!\n");
 
 if (CloseHandle(hThread) != 0)
-
 printf("Handle to thread closed successfully.\n");
 
 return 0;
-
 }
 
  
