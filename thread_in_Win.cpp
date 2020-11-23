4392722e6791118a4f696ca36ba132889f5a8b57
fahdolaf <46709034+fahdolaf@users.noreply.github.com>
2020-11-24 00:17:45 +0200

    Update thread_in_Win.cpp

--- thread_in_Win.cpp
+++ thread_in_Win.cpp
@@ -8,51 +8,21 @@
  ============================================================================
  */
 
+#include <windows.h>
 
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<unistd.h>
-
-#include <stdio.h>
-#include <wchar.h>
-#include <string.h>
-#include <Windows.h>
-#include <sapi.h>
-
-ISpVoice *pVoice = NULL;
-
-void printSmoothly(wchar_t *Str);
-
-int main(void)
-{
-    if (FAILED(::CoInitialize(NULL)))
-        return FALSE;
-
-    HRESULT hr = CoCreateInstance(CLSID_SpVoice, NULL, CLSCTX_ALL, 
-                    IID_ISpVoice, (void **)&pVoice);
-
-    wchar_t *sentence = L"Greetings professor falken,What would you like to do ?";
-
-    // how can i execute these two at the same time ?
-pVoice->Speak(sentence, SPF_ASYNC, NULL);
-printSmoothly(sentence);
-
-    pVoice->Release();
-
-    CoUninitialize();
-
-    return 0;
+DWORD WINAPI ThreadFunc(void* data) {
+  // Do stuff.  This will be the first function called on the new thread.
+  // When this function returns, the thread goes away.  See MSDN for more details.
+ printf("\n First thread processing\n");
+  return 0;
 }
 
-void printSmoothly(wchar_t *Str)
-{
-    size_t len = wcslen( Str ) , n ;
+int main() {
+ 	printf("\n========= Main ============\n" );
 
-    for( n = 0 ; n < len ; n++ )
-    {
-        wprintf( L"%c", Str[n] );
-
-        Sleep(50);
-    }
+  HANDLE thread = CreateThread(NULL, 0, ThreadFunc, NULL, 0, NULL);
+  if (thread) {
+    // Optionally do stuff, such as wait on the thread.
+   printf("Thread creation failed: %d\n", thread);
+  }
 }
