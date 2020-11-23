753bd90f734246544abda6375fa71e0e7f5b5fa6
fahdolaf <46709034+fahdolaf@users.noreply.github.com>
2020-11-24 01:03:30 +0200

    Update thread_in_Win_2.cpp

--- thread_in_Win_2.cpp
+++ thread_in_Win_2.cpp
@@ -1,13 +1,23 @@
 #include <windows.h>
 #include <stdio.h>
 #include <conio.h>
+#include <iostream>
 
 DWORD WINAPI MyThreadFunction(LPVOID lpParam){
 printf("The parameter: %lu.\n", *(DWORD*)lpParam);
 return 0;
 }
 
-int main(void){
+
+DWORD WINAPI myThread(LPVOID lpParameter)
+{
+	unsigned int& myCounter = *((unsigned int*)lpParameter);
+	while(myCounter < 0xFFFFFFFF) ++myCounter;
+	return 0;
+}
+
+int main(int argc, char* argv[])
+{
 DWORD dwThreadId, dwThrdParam = 1;
 HANDLE hThread;
 hThread = CreateThread(
@@ -29,7 +39,22 @@ printf("It seems the CreateThread() is OK lol!\n");
 
 if (CloseHandle(hThread) != 0)
 printf("Handle to thread closed successfully.\n");
+/////////////////////////////////////////////////////////////////
+ 
+ 	using namespace std;
 
+	unsigned int myCounter = 0;
+	DWORD myThreadID;
+	HANDLE myHandle = CreateThread(0, 0, myThread, &myCounter;, 0, &myThreadID;);
+	char myChar = ' ';
+	while(myChar != 'q') {
+		cout << myCounter << endl;
+		myChar = getchar();
+	}
+	
+	CloseHandle(myHandle);
+ 
+ 
 return 0;
 }
 
