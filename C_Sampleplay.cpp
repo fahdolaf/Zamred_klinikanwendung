4ced4cb423d7a90f2c27c9c822fb99e318268dce
fahdolaf <46709034+fahdolaf@users.noreply.github.com>
2020-11-23 22:33:11 +0200

    Update C_Sampleplay.cpp

--- C_Sampleplay.cpp
+++ C_Sampleplay.cpp
@@ -10,18 +10,27 @@
 
 #include "ChocolateyInstaller.h"
 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <pthreads.h>
-
-void* calls(void* ptr) 
-{ 
-    // using pthread_self() get current thread id 
-    printf("In function \nthread id = %d\n", pthread_self()); 
-    pthread_exit(NULL); 
-    return NULL; 
-} 
-
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+  
+#define NUM_OF_THREADS 2
+  
+pthread_t thread_id[NUM_OF_THREADS];
+  
+void* shared_function_between_two_threads(void *arg) {
+        unsigned long i = 0;
+        pthread_t id = pthread_self();
+  
+        if(pthread_equal(id, thread_id[0]))
+                printf("\n First thread processing\n");
+        else
+                printf("\n Second thread processing\n");
+        return NULL;
+}
+  
 int main() {
 
 	printf("\n========= Main ============\n" );
@@ -29,12 +38,24 @@ int main() {
 	//ChocolateyInstaller chocolateyInstaller;
 	//chocolateyInstaller.installAPPs();
 	//chocolateyInstaller.init();
-	pthread_t thread; // declare thread 
-	pthread_create(&thread, NULL, calls, NULL); 
-	printf("In main \nthread id = %d\n", thread);  
-	pthread_join(thread, NULL);  
-	
-	return 0;
+        int i = 1;
+        int err;
+  
+        for (i=0; i<NUM_OF_THREADS; i++) {
+                err = pthread_create(&(thread_id[i]), NULL, &shared_function_between_two_threads, NULL);
+                if (err != 0)
+                        printf("Thread creation failed :[%s]", strerror(err));
+                else
+                        printf("Thread %ld created successfully\n", thread_id[i]);
+        }
+  
+        printf("Waiting in main for thread to finish\n");
+        for (i=0; i<NUM_OF_THREADS; i++) {
+                pthread_join(thread_id[i], NULL);
+        }
+  
+        printf("Thread execution finished, exiting from main program\n");
+        return 0;
 }
 
 
