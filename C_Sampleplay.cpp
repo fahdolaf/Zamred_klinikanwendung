dc036cc6e147816c5de86578e0cb5269aaa37b74
fahdolaf <46709034+fahdolaf@users.noreply.github.com>
2020-11-23 22:24:14 +0200

    Update C_Sampleplay.cpp

--- C_Sampleplay.cpp
+++ C_Sampleplay.cpp
@@ -10,16 +10,30 @@
 
 #include "ChocolateyInstaller.h"
 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <pthread.h> 
 
+void* calls(void* ptr) 
+{ 
+    // using pthread_self() get current thread id 
+    printf("In function \nthread id = %d\n", pthread_self()); 
+    pthread_exit(NULL); 
+    return NULL; 
+} 
 
 int main() {
 
 	printf("\n========= Main ============\n" );
 
-	ChocolateyInstaller chocolateyInstaller;
+	//ChocolateyInstaller chocolateyInstaller;
 	//chocolateyInstaller.installAPPs();
-	chocolateyInstaller.init();
-
+	//chocolateyInstaller.init();
+	pthread_t thread; // declare thread 
+	pthread_create(&thread, NULL, calls, NULL); 
+	printf("In main \nthread id = %d\n", thread);  
+	pthread_join(thread, NULL);  
+	
 	return 0;
 }
 
