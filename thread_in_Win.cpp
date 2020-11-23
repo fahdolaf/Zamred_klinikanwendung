39b86e64133cef9b251c1a3f40077e7b8ae7f50f
fahdolaf <46709034+fahdolaf@users.noreply.github.com>
2020-11-24 00:12:32 +0200

    Update thread_in_Win.cpp

--- thread_in_Win.cpp
+++ thread_in_Win.cpp
@@ -14,12 +14,45 @@
 #include<stdlib.h>
 #include<unistd.h>
 
-int main() {
+#include <stdio.h>
+#include <wchar.h>
+#include <string.h>
+#include <Windows.h>
+#include <sapi.h>
 
-	printf("\n========= Main ============\n" );
+ISpVoice *pVoice = NULL;
 
-	
-  
-  printf("Thread execution finished, exiting from main program\n");
-  return 0; 
+void printSmoothly(wchar_t *Str);
+
+int main(void)
+{
+    if (FAILED(::CoInitialize(NULL)))
+        return FALSE;
+
+    HRESULT hr = CoCreateInstance(CLSID_SpVoice, NULL, CLSCTX_ALL, 
+                    IID_ISpVoice, (void **)&pVoice);
+
+    wchar_t *sentence = L"Greetings professor falken,What would you like to do ?";
+
+    // how can i execute these two at the same time ?
+pVoice->Speak(sentence, SPF_ASYNC, NULL);
+printSmoothly(sentence);
+
+    pVoice->Release();
+
+    CoUninitialize();
+
+    return 0;
+}
+
+void printSmoothly(wchar_t *Str)
+{
+    size_t len = wcslen( Str ) , n ;
+
+    for( n = 0 ; n < len ; n++ )
+    {
+        wprintf( L"%c", Str[n] );
+
+        Sleep(50);
+    }
 }
